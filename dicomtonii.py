71bad07a326fa29cb50ba70acd2f0583addb1fb4
potis <Korfiatis.Panagiotis@mayo.edu>
2017-04-02 19:45:53 -0500

    Add new service to convert dicoms to nii

---
+++ dicomtonii.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+from time import time
+import argparse
+import glob
+import shutil
+import os
+import string
+import random
+import subprocess
+
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+
+def converter(input_dicom,input_zip=[],output='output'):
+	# Create temporary directories
+	cwd = os.getcwd()
+	print 'Current directory'
+	print cwd
+	outputfolder=output
+	if not os.path.exists(outputfolder):
+		os.makedirs(outputfolder)
+	# Get all the data based on the search key-- these data consist of the nifti file and a zip version of the original nifti
+	command='plastimatch convert --input %s --output-img %s'%(input_dicom,outputfolder)
+	print 'Running'
+	print  command
+	subprocess.call(command, shell=True)
+	# shutil.make_archive(output, 'zip', outputfolder)
+	return 0
+
+def main(argv):
+	converter(argv.input_dicom,argv.output)
+	return 0
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser( description='Convert between dicom and nifti files.')
+	parser.add_argument ("-i", "--input_dicom",  help="nitifile input" , required=True)
+	parser.add_argument ("-o", "--output",  help="nitifile input" , required=False)
+	parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+	parser.add_argument("-q", "--quiet",
+						  action="store_false", dest="verbose",
+						  default=True,
+						  help="don't print status messages to stdout")
+	args = parser.parse_args()
+	main(args)
